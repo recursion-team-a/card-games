3c0f6f4f61b75265530f7954b0fde1a8e6896ab1
K4-R4 <mabdman512@gmail.com>
2023-06-06 00:04:40 +0900

    test husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,8 @@ const path = require('path')
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
 
+const formatCommand = () => `npm run format`
+
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, formatCommand],
 }
