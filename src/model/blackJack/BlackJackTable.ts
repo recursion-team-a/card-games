172a715a98fcec68740c9bd89472dda357625d03
K4R4 <mabdman512@gmail.com>
2023-06-13 12:58:10 +0900

    basic methods

--- src/model/blackJack/BlackJackTable.ts
+++ src/model/blackJack/BlackJackTable.ts
@@ -1,24 +1,18 @@
 import Deck from '../common/Deck'
-import Player from '../common/Player'
+import GameDescision from '../common/GameDesicion'
 import Table from '../common/Table'
 import BlackjackPlayer from './BlackjackPlayer'
 
 export default class BlackJackTable extends Table {
-  protected players: Array<Player> = []
-  protected gamePhase: Array<string> = [
-    'betting',
-    'acting',
-    'evaluatingWinner',
-    'roundOver',
-    'endOfGame',
-  ]
+  protected players: Array<BlackjackPlayer> = []
   protected deck: Deck = new Deck('Blackjack')
+
   // playerのhandにpushするには、this.deck.drawOne()の戻り値がCardのみでなくてはならない
   // BlackjackPlayerクラスにaddHandメソッドを追加
   public assignPlayerHands(): void {
     for (let i = 0; i < 2; i++) {
       for (let player of this.players) {
-        if (player instanceof BlackjackPlayer) player.addHand(this.deck.drawOne())
+        player.addHand(this.deck.drawOne())
       }
     }
   }
@@ -29,13 +23,52 @@ export default class BlackJackTable extends Table {
       player.initializeHandAndBet()
     }
   }
-  public evaluateMove(player: Player): void {}
-  public getTurnPlayer(): void {}
-  public haveTurn(): void {}
-  public evaluateAndGetRoundResults(): string {
-    return ''
+
+  public evaluateMove(player: BlackjackPlayer): void {
+    let gameDescision: GameDescision = player.promptPlayer()
+    if (gameDescision.action === 'hit') {
+      player.addHand(this.deck.drawOne())
+      if (player.getHandScore() > 21) player.gameStatus = 'butst'
+    }
+    if (gameDescision.action === 'bet') player.bet += gameDescision.amount
   }
-  public isBlackJack(player: Player): boolean {
-    return player.getHandScore() === 21
+
+  public getTurnPlayer(): BlackjackPlayer {
+    return this.players[this.turnCounter % this.playersSize]
+  }
+
+  public haveTurn(): void {
+    let currentPlayer: BlackjackPlayer = this.getTurnPlayer()
+    if (this.gamePhase === 'betting') {
+      this.evaluateMove(currentPlayer)
+      if (this.isLastPlayer(currentPlayer)) this.gamePhase = 'acting'
+    } else if (this.gamePhase === 'acting') {
+      while (!this.isAllPlayerActionResolved()) {
+        this.evaluateMove(currentPlayer)
+      }
+      if (this.isLastPlayer(currentPlayer)) this.gamePhase = 'evaluatingWinner'
+    } else if (this.gamePhase === 'evaluatingWinner') {
+      this.evaluateAndGetRoundResults()
+      this.clearPlayerHandsAndBet()
+      this.gamePhase = 'roundOver'
+    } else if (this.gamePhase === 'roundOver') {
+      this.gamePhase = 'endOfGame'
+    }
+    this.turnCounter += 1
+  }
+
+  public evaluateAndGetRoundResults(): string {}
+
+  public isBlackJack(player: BlackjackPlayer): boolean {
+    return player.hand.length == 2 && player.getHandScore() === 21
+  }
+
+  public isAllPlayerActionResolved(): boolean {
+    let resolved: boolean = true
+    for (let player of this.players) {
+      if (['broken', 'bust', 'stand'].findIndex((status) => player.gameStatus === status) === -1)
+        resolved = false
+    }
+    return resolved
   }
 }
--- src/model/common/Table.ts
+++ src/model/common/Table.ts
@@ -2,24 +2,60 @@ import Deck from './Deck'
 import Player from './Player'
 
 export default abstract class Table {
-  private betDominations: Array<number> = [5, 20, 50, 100]
-  private resultsLog: Array<string> = []
-  protected turnCounter: number = 0
-  protected abstract gamePhase: Array<string>
+  private static _betDominations: Array<number> = [5, 20, 50, 100]
+  private _resultsLog: Array<string>
+  private _turnCounter: number
+  private _gamePhase: string
   protected abstract deck: Deck
   protected abstract players: Array<Player>
 
-  constructor() {}
+  constructor(gamePhase: string) {
+    this._resultsLog = []
+    this._turnCounter = 0
+    this._gamePhase = gamePhase
+  }
+
+  get resultLog(): Array<string> {
+    return this._resultsLog
+  }
+
+  set resultLog(resultLog: Array<string>) {
+    this._resultsLog = this.resultLog
+  }
+
+  get turnCounter(): number {
+    return this._turnCounter
+  }
+
+  set turnCounter(turnCounter: number) {
+    this._turnCounter = turnCounter
+  }
+
+  get gamePhase(): string {
+    return this._gamePhase
+  }
+
+  set gamePhase(gamePhase: string) {
+    this.gamePhase = gamePhase
+  }
+
+  get playersSize(): number {
+    return this.players.length
+  }
+
   abstract assignPlayerHands(): void
+
   abstract clearPlayerHandsAndBet(): void
+
   abstract evaluateMove(player: Player): void
+
   abstract getTurnPlayer(): void
+
   abstract haveTurn(): void
+
   abstract evaluateAndGetRoundResults(): string
+
   public isLastPlayer(player: Player): boolean {
-    return (
-      this.players.findIndex((candidate) => candidate.name == player.name) ==
-      this.players.length - 1
-    )
+    return this.players.length > 0 && player === this.players[-1]
   }
 }
