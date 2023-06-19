11fcb0f0574743ad2b4cda5436b92553537f2977
K4R4 <mabdman512@gmail.com>
2023-06-19 21:33:35 +0900

    added warTable class

---
+++ src/model/war/WarTable.ts
@@ -0,0 +1,11 @@
+// import Deck from "../common/Deck";
+// import WarPlayer from "./WarPlayer";
+// import Table from "../common/Table";
+
+// class WarTable extends Table {
+// 	protected players: Array<WarPlayer> = [];
+
+// 	protected deck: Deck = new Deck('War');
+
+// 	constructor()
+// }
