3f736bb94cc824f8a88380b83d2b1579a6db36be
takatokawazu <ktakato38@gmail.com>
2023-06-28 00:43:15 -0700

    Merge branch 'develop' into feature/#56poker

--- src/model/common/Deck.ts
+++ src/model/common/Deck.ts
@@ -1,74 +1,72 @@
-// import Card from './Card'
+import Card from './Card'
 
-// export default class Deck {
-//   private cards: Card[]
+export default class Deck {
+  private cards: Card[]
 
-//   constructor(gameType?: string, playerType?: string) {
-//     // カードの配列
-//     this.cards = []
-//     // デッキの生成
-//     this.generateDeck(gameType, playerType)
-//     this.shuffle()
-//   }
+  constructor(gameType?: string, playerType?: string) {
+    // カードの配列
+    this.cards = []
+    // デッキの生成
+    this.generateDeck(gameType, playerType)
+    this.shuffle()
+  }
 
-//   // if(this.gameType == ??)等でゲームの種類に応じて変更可能
-//   // 現時点では, どのゲームも52枚で<List>cardsを生成
-//   generateDeck(gameType?: string, playerType?: string): void {
-//     const suits = ['♥︎', '♦', '♣', '♠']
-//     const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+  generateDeck(gameType?: string, playerType?: string): void {
+    const suits = ['♥︎', '♦', '♣', '♠']
+    const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-//     if (gameType === 'speed') {
-//       if (playerType === 'house') {
-//         for (let i = 0; i < suits.length - 2; i += 1) {
-//           for (let j = 0; j < ranks.length; j += 1) {
-//             this.cards.push(new Card(suits[i], ranks[j]))
-//           }
-//         }
-//       } else {
-//         for (let i = 2; i < suits.length; i += 1) {
-//           for (let j = 0; j < ranks.length; j += 1) {
-//             this.cards.push(new Card(suits[i], ranks[j]))
-//           }
-//         }
-//       }
-//     } else {
-//       for (let i = 0; i < suits.length; i += 1) {
-//         for (let j = 0; j < ranks.length; j += 1) {
-//           this.cards.push(new Card(suits[i], ranks[j]))
-//         }
-//       }
-//     }
-//   }
+    if (gameType === 'speed') {
+      if (playerType === 'house') {
+        for (let i = 0; i < suits.length - 2; i += 1) {
+          for (let j = 0; j < ranks.length; j += 1) {
+            this.cards.push(new Card(suits[i], ranks[j]))
+          }
+        }
+      } else {
+        for (let i = 2; i < suits.length; i += 1) {
+          for (let j = 0; j < ranks.length; j += 1) {
+            this.cards.push(new Card(suits[i], ranks[j]))
+          }
+        }
+      }
+    } else {
+      for (let i = 0; i < suits.length; i += 1) {
+        for (let j = 0; j < ranks.length; j += 1) {
+          this.cards.push(new Card(suits[i], ranks[j]))
+        }
+      }
+    }
+  }
 
-//   /*
-//         return null : デッキの状態を更新（ランダムに順番を入れ替える）
-//      */
-//   shuffle(): void {
-//     for (let i = this.cards.length - 1; i >= 0; i -= 1) {
-//       const randomIndex = Math.floor(Math.random() * (i + 1))
-//       const temp = this.cards[i]
-//       this.cards[i] = this.cards[randomIndex]
-//       this.cards[randomIndex] = temp
-//     }
-//   }
+  /*
+        return null : デッキの状態を更新（ランダムに順番を入れ替える）
+     */
+  shuffle(): void {
+    for (let i = this.cards.length - 1; i >= 0; i -= 1) {
+      const randomIndex = Math.floor(Math.random() * (i + 1))
+      const temp = this.cards[i]
+      this.cards[i] = this.cards[randomIndex]
+      this.cards[randomIndex] = temp
+    }
+  }
 
-//   /*
-//         return Card: ポップされたカードを返す.
-//      */
-//   drawOne(): Card | undefined {
-//     return this.cards.pop()
-//   }
+  /*
+        return Card: ポップされたカードを返す.
+     */
+  drawOne(): Card | undefined {
+    return this.cards.pop()
+  }
 
-//   /*
-//         return null: デッキをリセットする.
-//      */
-//   resetDeck(): void {
-//     this.cards = []
-//     this.generateDeck()
-//     this.shuffle()
-//   }
+  /*
+        return null: デッキをリセットする.
+     */
+  resetDeck(): void {
+    this.cards = []
+    this.generateDeck()
+    this.shuffle()
+  }
 
-//   getDeckSize(): number {
-//     return this.cards.length
-//   }
-// }
+  getDeckSize(): number {
+    return this.cards.length
+  }
+}
